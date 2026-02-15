3a9d0beb7518238e2d6ff202f1761bb252a258dd
IslamLessom <mem.m.1979@mail.ru>
2026-02-15 18:04:35 +0300

    edit

--- frontend/apps/admin-panel/src/pages/statistics-customers/ui/StatisticsCustomers.tsx
+++ frontend/apps/admin-panel/src/pages/statistics-customers/ui/StatisticsCustomers.tsx
@@ -319,7 +319,7 @@ export const StatisticsCustomers = () => {
                     borderRadius: '8px',
                     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)'
                   }}
-                  formatter={(value: number) => [value, 'Клиентов']}
+                  formatter={(value?: number) => [value, 'Клиентов']}
                 />
                 <Legend
                   wrapperStyle={{ paddingTop: '16px' }}
